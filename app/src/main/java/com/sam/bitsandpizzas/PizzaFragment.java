91ec58a33e639482986f49608469417fb90d0f3a
Caio-Santana <caio.santana@live.com>
2023-10-10 19:59:10 -0300

    Add pizza fragment, RecyclerView, pizza

--- app/src/main/java/com/sam/bitsandpizzas/PizzaFragment.java
+++ app/src/main/java/com/sam/bitsandpizzas/PizzaFragment.java
@@ -2,14 +2,15 @@ package com.sam.bitsandpizzas;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.ListFragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
-public class PizzaFragment extends ListFragment {
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PizzaFragment extends Fragment {
 
     public PizzaFragment() {
     }
@@ -18,13 +19,23 @@ public class PizzaFragment extends ListFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                inflater.getContext(),
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pizzas));
+        RecyclerView pizzaRecycler = (RecyclerView) inflater
+                .inflate(R.layout.fragment_pizza, container, false);
 
-        setListAdapter(adapter);
+        String[] pizzaNames = new String[Pizza.pizzas.length];
+        int[] pizzaImages = new int[Pizza.pizzas.length];
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        for (int i = 0; i < Pizza.pizzas.length; i++) {
+            pizzaNames[i] = Pizza.pizzas[i].getName();
+            pizzaImages[i] = Pizza.pizzas[i].getImageResourceId();
+        }
+
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pizzaNames, pizzaImages);
+        pizzaRecycler.setAdapter(adapter);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        pizzaRecycler.setLayoutManager(layoutManager);
+
+        return pizzaRecycler;
     }
 }
